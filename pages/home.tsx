7de7b8fb462cf7e11a30096f96c42f1f8a12ba6e
lowliet64 <kaduifrn@gmail.com>
2021-04-19 22:27:30 -0300

    styles: add skeleton in home page

--- pages/home.tsx
+++ pages/home.tsx
@@ -14,32 +14,98 @@ export default function Home() {
         }).catch(err => console.log(err))
     }, [listVideos])
 
-    return (
-        <div>
-            <Header></Header>
-            <div className="container-start">
-                {listVideos.map(v =>
-                    <div className="video-card">
-                        <div className="video-thumb">
-                            <a href={`https://www.youtube.com/${v}`}>
-                                <div className="time-biding">20:00</div>
-                                <img src="https://comofazer.site/wp-content/uploads/2021/02/COMO-JOGAR-LANE-PHASE-CONTRA-VOLIBEAR-Video-1024x576.jpg" alt="" className="image-thumb" />
-                            </a>
-                        </div>
-                        <div className="video-detail">
-                            <div><img src="https://yt3.ggpht.com/ytc/AAUvwnhw3VyX4fn24yWHirFxL9HnwBh4TP8uur1pFOEL=s88-c-k-c0x00ffffff-no-rj" alt="" className="profile-pic" /></div>
-                            <div className="detail-text">
-                                <h3>COMO JOGAR LANE PHASE CONTRA VOLIBEAR [Vídeo]</h3>
-                                <h4 style={{ fontWeight: 'initial', fontSize: '14px' }}>Olkaida</h4>
-                                <h4 style={{ fontWeight: 'initial', fontSize: '14px' }}>11 mi de visualizações  - há 1 ano</h4>
+
+    if (listVideos.length != 0) {
+        return (
+            <div>
+                <Header></Header>
+                <div className="container-start">
+                    {listVideos.map(v =>
+                        <div className="video-card">
+                            <div className="video-thumb">
+                                <a href={`https://www.youtube.com/${v}`}>
+                                    <div className="time-biding">20:00</div>
+                                    <img src="https://comofazer.site/wp-content/uploads/2021/02/COMO-JOGAR-LANE-PHASE-CONTRA-VOLIBEAR-Video-1024x576.jpg" alt="" className="image-thumb" />
+                                </a>
+                            </div>
+                            <div className="video-detail">
+                                <div><img src="https://yt3.ggpht.com/ytc/AAUvwnhw3VyX4fn24yWHirFxL9HnwBh4TP8uur1pFOEL=s88-c-k-c0x00ffffff-no-rj" alt="" className="profile-pic" /></div>
+                                <div className="detail-text">
+                                    <h3>COMO JOGAR LANE PHASE CONTRA VOLIBEAR [Vídeo]</h3>
+                                    <h4 style={{ fontWeight: 'initial', fontSize: '14px' }}>Olkaida</h4>
+                                    <h4 style={{ fontWeight: 'initial', fontSize: '14px' }}>11 mi de visualizações  - há 1 ano</h4>
+                                </div>
                             </div>
                         </div>
+
+                    )}
+
+
+                </div>
+            </div>
+
+        )
+
+    } else {
+
+        return (
+            <div>
+                <Header></Header>
+                <div className="container-start">
+                    <div className="card">
+                        <div className="card__image loading"></div>
+                        <div className="card__title loading"></div>
+                        <div className="card__description loading"></div>
                     </div>
 
-                )}
 
+                    <div className="card">
+                        <div className="card__image loading"></div>
+                        <div className="card__title loading"></div>
+                        <div className="card__description loading"></div>
+                    </div>
+
+
+                    <div className="card">
+                        <div className="card__image loading"></div>
+                        <div className="card__title loading"></div>
+                        <div className="card__description loading"></div>
+                    </div>
+                    <div className="card">
+                        <div className="card__image loading"></div>
+                        <div className="card__title loading"></div>
+                        <div className="card__description loading"></div>
+                    </div>
+
+                    <div className="card">
+                        <div className="card__image loading"></div>
+                        <div className="card__title loading"></div>
+                        <div className="card__description loading"></div>
+                    </div>
+
+
+                    <div className="card">
+                        <div className="card__image loading"></div>
+                        <div className="card__title loading"></div>
+                        <div className="card__description loading"></div>
+                    </div>
+
+
+                    <div className="card">
+                        <div className="card__image loading"></div>
+                        <div className="card__title loading"></div>
+                        <div className="card__description loading"></div>
+                    </div>
+                    <div className="card">
+                        <div className="card__image loading"></div>
+                        <div className="card__title loading"></div>
+                        <div className="card__description loading"></div>
+                    </div>
+
+                </div>
 
             </div>
-        </div>
-    )
+        )
+    }
+
 }
