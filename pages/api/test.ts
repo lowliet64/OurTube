0bc69b570f3fcecbcaed8eadc3fbab1c0cba0bc9
Carlos  Silva <30802810+lowliet64@users.noreply.github.com>
2021-04-19 21:41:15 -0300

    Update test.ts

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,1 +1,9 @@
 
+
+import { NextApiRequest, NextApiResponse } from "next"
+
+
+export default function (req: NextApiRequest, res: NextApiResponse) {
+            return res.status(200).send({"status":"ok"})
+   
+}
